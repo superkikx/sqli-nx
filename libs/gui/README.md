341659d633b3c60ab7bb5a792a758d8c062cc889
freezeburger <renaud.dubuis@decryptage.net>
2019-10-28 17:10:11 +0100

    GUI Library

---
+++ libs/gui/README.md
@@ -0,0 +1,7 @@
+# gui
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test gui` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/gui/src/lib/gui.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./gui.css";
+
+/* eslint-disable-next-line */
+export interface GuiProps {}
+
+export const Gui = (props: GuiProps) => {
+  return (
+    <div>
+      <h1>Welcome to gui component!</h1>
+    </div>
+  );
+};
+
+export default Gui;
