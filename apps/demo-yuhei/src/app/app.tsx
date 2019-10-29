9aa8f72288b1ba857c54c7732b6bd42f2be12481
Yuhei Nakata <yuh.nakata@gmail.com>
2019-10-29 15:04:39 +0100

    add cta label

--- apps/demo-yuhei/src/app/app.tsx
+++ apps/demo-yuhei/src/app/app.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import "./app.css";
 import {Text} from '@sqli/gui'
+import {AudioPlayer} from '@sqli/gui'
+import {BreadcrumbSqli} from '@sqli/gui'
 
 export const App = () => {
   /*
@@ -9,28 +11,21 @@ export const App = () => {
    *
    * Note: The corresponding styles are in the ./${fileName}.${style} file.
    * 
-   *  <Text>
-        {{
-          title: "Fishipedia",
-          subtitle: "L'encylopédie du monde aquatique",
-          href: "https://www.fishipedia.fr/",
-          content: '“Fishipedia.fr - 1er site encyclopédique dédié aux poissons et aux passions associées : Plongée, Snorkeling, Aquariophilie, Pêche.”',
-          imageSrc: 'https://www.fishipedia.fr/wp-content/themes/fishipedia/css/img/home/thematics/fishes.jpg'
-        }}
-      </Text>
-   * 
    */
   return (
     <div className="app">
+      <BreadcrumbSqli />
       <Text>
         {{
           title: "Fishipedia",
           subtitle: "L'encylopédie du monde aquatique",
           href: "https://www.fishipedia.fr/",
+          ctaTitle: "visiter le site",
           content: '“Fishipedia.fr - 1er site encyclopédique dédié aux poissons et aux passions associées : Plongée, Snorkeling, Aquariophilie, Pêche.”',
           imageSrc: 'https://www.fishipedia.fr/wp-content/themes/fishipedia/css/img/home/thematics/fishes.jpg'
         }}
       </Text>
+      <AudioPlayer/>
       <Text>
         {{
           subtitle: "L'encylopédie du monde aquatique",
