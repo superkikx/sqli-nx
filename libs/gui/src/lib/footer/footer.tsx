f1fbbf59cbe085dbcd35d831030bd57cc8af476e
Abdeljelil Najmeddine <nabdeljelil@L530439.SQLI>
2019-10-29 11:22:09 +0100

    demo-Abdeljelil

---
+++ libs/gui/src/lib/footer/footer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./footer.css";
+
+
+export const Footer = (props) => {
+  return (
+    <div>
+      <h1>Welcome to newFooter component!</h1>
+    </div>
+  );
+};
+
+export default Footer;
