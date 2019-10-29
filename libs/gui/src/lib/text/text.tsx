03ebe8f81742e5edc35706dcb28d15865e18ce95
Yuhei Nakata <yuh.nakata@gmail.com>
2019-10-29 15:26:59 +0100

    add reversed option

--- libs/gui/src/lib/text/text.tsx
+++ libs/gui/src/lib/text/text.tsx
@@ -16,27 +16,31 @@ import "./text.css";
  */
 export const Text = (props) => {
   return (
-    <div className="card 1 flip-card">
-      <div className="flip-card-inner">
-        <div className="flip-card-front">
-          <div className="card_image"> 
-            {props.children.imageSrc && <img src={props.children.imageSrc} />}
-            {!props.children.imageSrc && <img src="https://i.redd.it/b3esnz5ra34y.jpg" />}
+    <section>
+      <div className={"card 1 flip-card " + (props.children.reversed && 'reversed')}>
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <div className="card_image"> 
+              {props.children.imageSrc && <img src={props.children.imageSrc} />}
+              {!props.children.imageSrc && <img src="https://i.redd.it/b3esnz5ra34y.jpg" />}
+            </div>
+            <div className="card_title title-white">
+              {props.children.title && <p>{props.children.title}</p>}
+            </div>
           </div>
-          <div className="card_title title-white">
-            {props.children.title && <p>{props.children.title}</p>}
+          <div className="flip-card-back">
+              {props.children.content && 
+              <p>
+              {props.children.content}
+              {props.children.href && 
+                <a href={props.children.href}> 
+                  {props.children.ctaTitle}
+                </a>}
+              </p>}
           </div>
-        </div>
-        <div className="flip-card-back">
-            {props.children.content && 
-            <p>
-            {props.children.content}
-            {props.children.href && <a href={props.children.href}>{props.children.ctaTitle}</a>}
-            </p>}
-            
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
