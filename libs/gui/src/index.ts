86f53567d143528f7dcfb93bf49c0aaa9eaa16bd
Ladib Khalid <kladib@sqli.com>
2019-10-29 12:11:14 +0100

    Fix Index TS

--- libs/gui/src/index.ts
+++ libs/gui/src/index.ts
@@ -4,6 +4,5 @@ export * from "./lib/autocomplete/autocomplete";
 export * from "./lib/breadcrumb/breadcrumb";
 export * from "./lib/text/text";
 export * from "./lib/demo/demo";
-export * from "./lib/button/button";
 export * from "./lib/logo/logo";
 export * from "./lib/audioplayer/audio-player";
