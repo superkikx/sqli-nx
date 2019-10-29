303430b86c32e6ea104fa0b7c1c356f0e8fffb7a
Maximilien <icart.maximilien@gmail.com>
2019-10-29 15:14:47 +0100

    [MAX] - New design

--- apps/demo-max/src/app/app.tsx
+++ apps/demo-max/src/app/app.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 import "./app.css";
-import {AudioPlayer, BreadcrumbSqli} from "@sqli/gui";
+import {AudioPlayer, BreadcrumbSqli, Text} from "@sqli/gui";
 
 const data = [
-    {
-        action:'http://www.rorymcilroy.com/',
-        label:'Rory Mcilroy'
-    },
-    {
-        action:'https://twitter.com/kikxmachine',
-        label:'Superkikx'
-    },
-    {
-        action:'https://tigerwoods.com/',
-        label:'Tiger Woods'
-    }
+        {
+            action:'http://www.rorymcilroy.com/',
+            label:'Rory Mcilroy'
+        },
+        {
+            action:'https://twitter.com/kikxmachine',
+            label:'Superkikx'
+        },
+        {
+            action:'https://tigerwoods.com/',
+            label:'Tiger Woods'
+        }
     ];
 
 
@@ -23,6 +23,15 @@ export const App = () => {
   return (
     <div className="app">
       <BreadcrumbSqli trail={data}/>
+        <Text>
+            {{
+                title: "Fishipedia",
+                subtitle: "L'encylopédie du monde aquatique",
+                href: "https://www.fishipedia.fr/",
+                content: '“Fishipedia.fr - 1er site encyclopédique dédié aux poissons et aux passions associées : Plongée, Snorkeling, Aquariophilie, Pêche.”',
+                imageSrc: 'https://www.fishipedia.fr/wp-content/themes/fishipedia/css/img/home/thematics/fishes.jpg'
+            }}
+        </Text>
       <AudioPlayer coverImage={''}/>
     </div>
   );
