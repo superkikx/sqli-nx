6329c17bcb3ca9d63e0d5939fcb935e0a7f004fd
Abdeljelil Najmeddine <nabdeljelil@L530439.SQLI>
2019-10-29 15:01:31 +0100

    respensive footer

---
+++ libs/gui/src/lib/footer/footer.props.tsx
@@ -0,0 +1,8 @@
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    background: PropTypes.oneOf(['gray','crimson','orange']),
+    children: PropTypes.string
+};
+
+export default propTypes
