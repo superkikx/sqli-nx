f0e6f56ecedb875b6b37fba7127a390af36258f7
freezeburger <renaud.dubuis@decryptage.net>
2019-10-29 10:21:02 +0100

    Remove Unused App

--- apps/demo-audio-player-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from "../support/app.po";
-
-describe("demo-audio-player", () => {
-  beforeEach(() => cy.visit("/"));
-
-  it("should display welcome message", () => {
-    getGreeting().contains("Welcome to demo-audio-player!");
-  });
-});
