be6f892840ca8e871163e13c80b608a577447475
Tobias <stobias@sqli.com>
2019-10-29 10:21:15 +0100

    Named Export

--- libs/gui/src/lib/audioplayer/audio-player.spec.tsx
+++ libs/gui/src/lib/audioplayer/audio-player.spec.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "@testing-library/react";
 
-import AudioPlayer from "./audio-player";
+import { AudioPlayer } from "./audio-player";
 
 describe(" AudioPlayer", () => {
   it("should render successfully", () => {
