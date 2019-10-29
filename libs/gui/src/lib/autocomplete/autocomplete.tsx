30b24cbacbb99116a2a3ef0a119ef37028882764
freezeburger <renaud.dubuis@decryptage.net>
2019-10-29 16:43:40 +0100

    Action for Autocomplete

--- libs/gui/src/lib/autocomplete/autocomplete.tsx
+++ libs/gui/src/lib/autocomplete/autocomplete.tsx
@@ -8,7 +8,6 @@ export const Autocomplete = (props) => {
   const [input,setInput] = React.useState(props.input || '');
 
   const changeHandler = evt => {
-    // console.log(evt.target.value);
     setInput(evt.target.value);
   }
 
@@ -22,7 +21,8 @@ export const Autocomplete = (props) => {
           className="form-control mdb-autocomplete"
           value={input}
           onChange={changeHandler} 
-          placeholder={props.placeholder}/>
+          placeholder={props.placeholder}
+          onBlur={ () => (typeof props.action === 'function') && props.action(input) }/>
           <div>
           {input && props.data.filter( value => value.startsWith(input)).map( (value,num) => <div key={num} onClick={ ()=> setInput(value) }>{value}<hr/></div>)}
           </div>
