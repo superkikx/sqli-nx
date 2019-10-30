10fe60ddaf0992056dc354e1d1dc7fe0c0249bb2
freezeburger <renaud.dubuis@decryptage.net>
2019-10-30 16:22:31 +0100

    Update AutoComplete

--- apps/demo-renaud/src/app/container/home.tsx
+++ apps/demo-renaud/src/app/container/home.tsx
@@ -69,6 +69,9 @@ import {
       reversed:false
     });
 
+    const searchHandler = data => {
+      console.log(data);
+    };
 
     return (
       <div className="app col">
@@ -97,10 +100,13 @@ import {
               trail={trail}
             ></BreadcrumbSqli>
             <div className="pad">
+              
               <Autocomplete
                 placeholder="Type Your Favorite Music Name"
-                data={['Test']}
+                action={searchHandler}
+                data={ tracks.map( track => track.name )}
               />
+
               <Button>Validate</Button>
             </div>
             <div className="pad wrap slide">
