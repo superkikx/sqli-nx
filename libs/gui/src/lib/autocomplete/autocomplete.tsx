1254c7293c4976f98af981b16c75c3eabcfe257b
freezeburger <renaud.dubuis@decryptage.net>
2019-10-30 16:24:18 +0100

    Update AutoComplete

--- libs/gui/src/lib/autocomplete/autocomplete.tsx
+++ libs/gui/src/lib/autocomplete/autocomplete.tsx
@@ -23,11 +23,12 @@ export const Autocomplete = (props) => {
           value={input}
           onChange={changeHandler} 
           placeholder={props.placeholder}
-          onBlur={ () => (typeof props.action === 'function') && props.action(input) }/>
+          />
           <div>
           {input && props.data.filter( value => value.startsWith(input)).map( (value,num) => <div key={num} onClick={ ()=> {
+            
+            (typeof props.action === 'function') && props.action(value)
             setInput(value); 
-            (typeof props.action === 'function') && props.action(input)
           }}>{value}<hr/></div>)}
           </div>
         </div>
