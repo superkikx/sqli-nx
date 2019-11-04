ffcf16a0616dd6bdddfac64e9a8aafc78e093e64
Maximilien <icart.maximilien@gmail.com>
2019-11-04 10:19:05 +0100

    [MAX] - Clean arbo

--- apps/demo-max-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from "../support/app.po";
-
-describe("demo-max", () => {
-  beforeEach(() => cy.visit("/"));
-
-  it("should display welcome message", () => {
-    getGreeting().contains("Welcome to demo-max!");
-  });
-});
--- apps/momo-demo-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from "../support/app.po";
-
-describe("momo-demo", () => {
-  beforeEach(() => cy.visit("/"));
-
-  it("should display welcome message", () => {
-    getGreeting().contains("Welcome to momo-demo!");
-  });
-});
